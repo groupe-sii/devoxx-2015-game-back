cb8a81451c4b63649afc7f54a3c1b3c4d1479341
Cedric Le Gallo <clegallo@sii.fr>
2015-03-10 14:27:25 +0100

    RollBack post demo

--- survival-game/src/main/java/fr/sii/survival/WebSocketConfig.java
+++ survival-game/src/main/java/fr/sii/survival/WebSocketConfig.java
@@ -36,10 +36,7 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint(ENDPOINT_PREFIX).withSockJS()
-		 .setClientLibraryUrl("http://10.6.192.65:8000/app/bower_components/sockjs/sockjs.js")
-		 .setClientLibraryUrl("http://10.6.192.65:8000/app/bower_components/sockjs/sockjs.min.js")
-		 .setClientLibraryUrl("http://10.6.192.65:8000/sockjs.min.js");
+		registry.addEndpoint(ENDPOINT_PREFIX).withSockJS();
 	}
 
 	/*
