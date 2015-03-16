97b50eba646698b6ace6ea842e98be327606529e
abaudet <abaudet@sii.fr>
2015-03-16 18:20:53 +0100

    - oups...

--- survival-game/src/main/java/fr/sii/survival/controller/ErrorController.java
+++ survival-game/src/main/java/fr/sii/survival/controller/ErrorController.java
@@ -1,7 +1,6 @@
 package fr.sii.survival.controller;
 
 import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
-import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.annotation.SendToUser;
 
 import fr.sii.survival.core.domain.message.Error;
@@ -9,7 +8,6 @@ import fr.sii.survival.core.domain.message.Error;
 // TODO: This controller won't work standalone... It must be used by inheritance due to error handler search limited to current class only
 public class ErrorController {
 	@MessageExceptionHandler
-	@MessageMapping
 	@SendToUser(value = "/queue/errors", broadcast = false)
 	public Error handleException(Throwable exception) {
 		return new Error(exception);
