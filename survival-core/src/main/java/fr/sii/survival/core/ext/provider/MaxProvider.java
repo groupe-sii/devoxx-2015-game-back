dacf8918f0666303b877770224eb776782267a04
abaudet <abaudet@sii.fr>
2015-04-06 14:03:35 +0200

    Fix max provider to enemies Add ImageException

--- survival-core/src/main/java/fr/sii/survival/core/ext/provider/MaxProvider.java
+++ survival-core/src/main/java/fr/sii/survival/core/ext/provider/MaxProvider.java
@@ -4,8 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import fr.sii.survival.core.domain.Game;
+import fr.sii.survival.core.domain.player.Enemy;
 import fr.sii.survival.core.exception.ExtensionInitializationException;
 import fr.sii.survival.core.ext.EnemyExtension;
+import fr.sii.survival.core.service.game.PlayerTypePredicate;
 
 /**
  * Provider that limits the number of enemies on the field. If the maximum
@@ -34,7 +36,7 @@ public class MaxProvider implements ExtensionProvider {
 
 	@Override
 	public List<EnemyExtension> getEnemies(Game game) throws ExtensionInitializationException {
-		if (max <= 0 || game.getPlayers().size() < max) {
+		if (max <= 0 || game.getPlayers(new PlayerTypePredicate(Enemy.class)).size() < max) {
 			return delegate.getEnemies(game);
 		} else {
 			return new ArrayList<>(0);
